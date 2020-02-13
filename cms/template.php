b9eb06b1b38d4de4563b8de770dd199b9912c5b5
Jirka <horakj7@gmail.com>
2020-02-13 15:25:15 +0100

    try fotorama dimens

--- cms/template.php
+++ cms/template.php
@@ -802,6 +802,9 @@ __EOD;
     $GoogleAnalytics
     $onLoad
   </script>
+  
+  <link  href="https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script>
 </head>
 __EOD;
 
