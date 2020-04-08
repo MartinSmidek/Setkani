e00d4b247d7eea37515c16d9a6166a58c61de302
Jirka <horakj7@gmail.com>
2020-04-08 15:26:05 +0200

    remove multiple H1 tags from main page

--- cms/template.php
+++ cms/template.php
@@ -517,6 +517,7 @@ function template($href,$path,$fe_host0,$fe_user0=0,$be_user0=0,$echo=1) { trace
       case 'home':    # ----------------------------------------------- . home
 //    $id= array_shift($path);
 //    list($id)= explode('#',$id);
+        $body.= "<div class='content'><h1>YMCA Setkání</h1></div>";
         $body.= home();
         if (!$CMS) {$body.= facebook();}
         break;
@@ -1394,7 +1395,7 @@ function home() { trace();
     $code= cid_pid($cid,$x->uid);
     //todo ugly, consider "main page" category
     if ( $x->page==100 ) { // ---------------------------------------- hlavní strana - úvodní článek & timeline
-      $telo.= vlakno($cid,'clanek','home',true);
+      $telo.= vlakno($cid,'clanek','home',false);
     }
     elseif ( $x->home==2 || $x->home==6 ) { // ----------------------- abstrakt na home | nahoru
       $prihlaska= '';
