b6b08abee451aa769e1a12a1981a5b79f01568e6
Jirka <horakj7@gmail.com>
2020-11-01 16:50:40 +0100

    still something wrong with the script

--- gmail_token.php
+++ gmail_token.php
@@ -44,87 +44,92 @@ if (!$be_allowed) {
         'ZAVŘÍT', "top.close();", 0);
 } else {
     // SETUP
-    $allowed_mails = array("objednavky-domu@setkani.org", "dum@setkani.org", "answer@setkani.org", "www.chlapi.cz@gmail.com");
-    $credentials_path = $_SERVER['DOCUMENT_ROOT'].'../files/setkani4/credential.json';
-    $required_privileges = array(
-        //"https://www.googleapis.com/auth/gmail.settings.basic", //to view email metadata
-        //"https://www.googleapis.com/auth/gmail.send" //to send emails
-        // OR
-        "https://mail.google.com/" //global privilege
-    );
-    $tokenPathPrefix = $_SERVER['DOCUMENT_ROOT'].'../files/setkani4/token_'; //path and token file prefix, email address will be appended
-    $tokenPathSuffix = '.json';
-    $delay = 5; //seconds to redirect
-    $email = $_SESSION["gmail_api_refresh_token"];
+    try {
+        $allowed_mails = array("objednavky-domu@setkani.org", "dum@setkani.org", "answer@setkani.org", "www.chlapi.cz@gmail.com");
+        $credentials_path = $_SERVER['DOCUMENT_ROOT'].'../files/setkani4/credential.json';
+        $required_privileges = array(
+            //"https://www.googleapis.com/auth/gmail.settings.basic", //to view email metadata
+            //"https://www.googleapis.com/auth/gmail.send" //to send emails
+            // OR
+            "https://mail.google.com/" //global privilege
+        );
+        $tokenPathPrefix = $_SERVER['DOCUMENT_ROOT'].'../files/setkani4/token_'; //path and token file prefix, email address will be appended
+        $tokenPathSuffix = '.json';
+        $delay = 5; //seconds to redirect
+        $email = $_SESSION["gmail_api_refresh_token"];
 
-    // FIRE
-    require_once $_SERVER['DOCUMENT_ROOT'].'/ezer3.1/server/licensed/google_api/vendor/autoload.php';
-    $client = new Google_Client();
-    $client->setAuthConfig($credentials_path);
-    $client->setPrompt("consent");
-    $client->setScopes($required_privileges);
-    $client->setAccessType('offline');
-    $client->setIncludeGrantedScopes(true);
+        // FIRE
+        require_once $_SERVER['DOCUMENT_ROOT'].'/ezer3.1/server/licensed/google_api/vendor/autoload.php';
+        $client = new Google_Client();
+        $client->setAuthConfig($credentials_path);
+        $client->setPrompt("consent");
+        $client->setScopes($required_privileges);
+        $client->setAccessType('offline');
+        $client->setIncludeGrantedScopes(true);
 
-    // Get new token - see redirect below
-    if (isset($_GET['code'])) {
-        $filePath = $tokenPathPrefix . $email . $tokenPathSuffix;
-        try {
-            $accessToken = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+        // Get new token - see redirect below
+        if (isset($_GET['code'])) {
+            $filePath = $tokenPathPrefix . $email . $tokenPathSuffix;
+            try {
+                $accessToken = $client->fetchAccessTokenWithAuthCode($_GET['code']);
 
-            // Save the token to a file.
-            if (!file_exists(dirname($filePath))) {
-                mkdir(dirname($filePath), 0700, true);
+                // Save the token to a file.
+                if (!file_exists(dirname($filePath))) {
+                    mkdir(dirname($filePath), 0700, true);
+                }
+
+                $client->setAccessToken($accessToken);
+                $service = new Google_Service_Gmail($client);
+                $me = $service->users->getProfile('me');
+                if ($email != $me->emailAddress) {
+                    throw new Exception("Špatná emailová adresa: '{$me->emailAddress}'. Přihlašte se jako uživatel '$email'.");
+                } else {
+                    //if (file_exists($filePath)) { echo "WARN: overriding existing token file $filePath"; }
+                    file_put_contents($filePath, json_encode($accessToken));
+                    displayToUser('Autentizace se zdařila.',
+                        'Můžete zavřít toto okno a pokračovat v odesílání emailů.',
+                        'ZAVŘÍT', "top.close();", 0);
+                    $_SESSION["gmail_api_refresh_token"] = "";
+                }
+            } catch (Exception $e) {
+                $error = $e->getMessage();
+                displayToUser('Autentizace selhala.', "<pre class='lds_code'><code>$error</code></pre>", 'OPAKOVAT',
+                    "window.location.href = 'https://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}';", 0);
             }
-
-            $client->setAccessToken($accessToken);
-            $service = new Google_Service_Gmail($client);
-            $me = $service->users->getProfile('me');
-            if ($email != $me->emailAddress) {
-                throw new Exception("Špatná emailová adresa: '{$me->emailAddress}'. Přihlašte se jako uživatel '$email'.");
+        } else  {
+            // CHECK MAIL
+            if (!in_array($email, $allowed_mails)) {
+                displayToUser('Nepovolený přístup.', 'Zadaná adresa \'$email\' není validní adresou pro autentizaci.',
+                    'ZAVŘÍT', 'top.close();', 0);
             } else {
-                //if (file_exists($filePath)) { echo "WARN: overriding existing token file $filePath"; }
-                file_put_contents($filePath, json_encode($accessToken));
-                displayToUser('Autentizace se zdařila.',
-                    'Můžete zavřít toto okno a pokračovat v odesílání emailů.',
-                    'ZAVŘÍT', "top.close();", 0);
-                $_SESSION["gmail_api_refresh_token"] = "";
+                // Get the token - redirect to the same page !! HTTP or HTTPS must be correctly set
+                $client->setLoginHint($email);
+                $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+                $client->setRedirectUri($redirect_uri);
+                $auth_url = $client->createAuthUrl();
+
+                displayToUser('Budete přesměrováni na Google.',
+                    "Přihlašte se jako uživatel <b>$email</b> a povolte aplikaci <i>Answer</i> práva k odesílání emailů.",
+                    'ZAVŘÍT', 'top.close();', $delay);
+
+                echo "<script type='text/javascript'>
+             var counter = $delay;
+             let counterText = document.getElementById('counterText');
+             setInterval(function(){
+               counter--;
+               counterText.innerHTML = counter + ' s';
+             },1000);
+             setTimeout(function(){
+                window.location.href = '$auth_url';
+             }, {$delay}000);
+             </script>";
             }
-        } catch (Exception $e) {
-            $error = $e->getMessage();
-            displayToUser('Autentizace selhala.', "<pre class='lds_code'><code>$error</code></pre>", 'OPAKOVAT',
-                "window.location.href = 'https://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}';", 0);
         }
-    } else  {
-        // CHECK MAIL
-        if (!in_array($email, $allowed_mails)) {
-            displayToUser('Nepovolený přístup.', 'Zadaná adresa \'$email\' není validní adresou pro autentizaci.',
-                'ZAVŘÍT', 'top.close();', 0);
-            exit;
-        }
-        // Get the token - redirect to the same page !! HTTP or HTTPS must be correctly set
-        $client->setLoginHint($email);
-        $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-        $client->setRedirectUri($redirect_uri);
-        $auth_url = $client->createAuthUrl();
-
-        displayToUser('Budete přesměrováni na Google.',
-            "Přihlašte se jako uživatel <b>$email</b> a povolte aplikaci <i>Answer</i> práva k odesílání emailů.",
-            'ZAVŘÍT', 'top.close();', $delay);
-
-        echo "<script type='text/javascript'>
-         var counter = $delay;
-         let counterText = document.getElementById('counterText');
-         setInterval(function(){
-           counter--;
-           counterText.innerHTML = counter + ' s';
-         },1000);
-         setTimeout(function(){
-            window.location.href = '$auth_url';
-         }, {$delay}000);
-         </script>";
+    } catch (Exception $ex) {
+        echo $ex;
     }
 }
+echo file_get_contents("php://stderr");
 echo "</body></html>";
 
 function displayToUser($title, $msg, $button_text, $button_js, $loader) {
